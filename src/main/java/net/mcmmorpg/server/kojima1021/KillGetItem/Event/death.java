5326e2cd902b541790c3db099b32f21ab531bef7
kojima1021 <kojima1021_sr@yahoo.co.jp>
2014-04-08 16:35:23 +0900

    いろいろ修正

--- src/main/java/net/mcmmorpg/server/kojima1021/KillGetItem/Event/death.java
+++ src/main/java/net/mcmmorpg/server/kojima1021/KillGetItem/Event/death.java
@@ -1,22 +1,28 @@
 package net.mcmmorpg.server.kojima1021.KillGetItem.Event;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 
+
 public class death implements Listener {
 
 	@EventHandler
     public void DeathEvent(PlayerDeathEvent e) {
+	String prefix = ChatColor.YELLOW + "["+  ChatColor.DARK_RED + "ForceServer" + ChatColor.YELLOW + "]" + ChatColor.RESET;
 	ItemStack[] item = e.getEntity().getInventory().getContents();
 	for (ItemStack itemstack : item) {
-		 if (itemstack != null)
-			 if (e.getEntity().getKiller() != null)
-		  e.getEntity().getKiller().getInventory().addItem(itemstack);
-		  e.getEntity().getInventory().clear();
+		 if (itemstack != null){
+			 if (e.getEntity().getKiller() != null){
+			     e.getEntity().getInventory().clear();
+		         Location pos =e.getEntity().getKiller().getLocation();
+		         e.getEntity().getKiller().getWorld().dropItem(pos,new ItemStack(itemstack));
+		
 		}
-
-    }
-
+		 }
+	}
+}
 }
